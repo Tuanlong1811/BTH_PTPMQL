b96473123e926cb2f58dd0e721586542ee912ea1
Long <ndtoa@DESKTOP-H92T6GA>
2023-03-06 15:28:41 +0700

    Baithuchanh0703

---
+++ BTH_2002/BTH_2002/Controllers/Studentcontroller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student.Models;
+
+
+namespace Student.Controllers
+{
+    public class Studentcontroller : Controller
+    {
+        public IActionResult Index(string StudentID, string StudentName , string Age)
+        {
+            ViewBag.ss = "Hello" + StudentID + "-" + StudentName + Age;
+            return View();
+        }
+
+        public IActionResult Create(Student.Models.Student std)
+        {
+            string message = std.StudentID + "-";
+            message += std.StudentName + "-";
+            message += std.Age + "-";
+            ViewBag.SS = message;
+            return View();
+        }
+
+    }
+}
