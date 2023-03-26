70a164c201e112cd38c4508b44affafe20748ce4
Long <ndtoa@DESKTOP-H92T6GA>
2023-03-26 19:15:23 +0700

    BTH_2003

---
+++ baithuchanh2003/baithuchanh2003/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace baithuchanh2003.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ baithuchanh2003/baithuchanh2003/Models/employee.cs
@@ -0,0 +1,9 @@
+﻿namespace BaiThucHanh2003.Models
+{
+    public class Employee
+    {
+        public string? EmployeeName { get; set; }
+        public string? PhoneNumber { get; set; }
+
+    }
+}
